f5c9d176f0f2d41e1940a14ff9eb5ccbc28aafae
IamA1536 <18974735866@163.com>
2018-09-26 17:43:47 +0800

    Fix bugs

--- ExerciseCSU/QuickSort.cpp
+++ ExerciseCSU/QuickSort.cpp
@@ -1,16 +1,5 @@
 #include"QuickSort.h"
 
-int main() {
-	int n;
-	int arr[100000];
-	std::cout << "Please input the number of array(no more than 100000): ";
-	std::cin >> n;
-	for (int i = 0; i < n; i++) std::cin >> arr[i];
-	QSort(arr, 0, n - 1);
-	for (int i = 0; i < n; i++) std::cout << arr[i];
-	return 0;
-}
-
 void QSort(int arr[], int low, int high) {
 	int l = low;
 	int h = high;
@@ -31,4 +20,18 @@ void QSort(int arr[], int low, int high) {
 		if (l > low) QSort(arr, low, l - 1);
 		if (h < high) QSort(arr, l + 1, high);
 	}
+}
+
+int main() {
+	int n;
+	int arr[100000];
+	std::cout << "Please input the number of array(no more than 100000): ";
+	std::cin >> n;
+	std::cout << "Please input the element of array: ";
+	for (int i = 0; i < n; i++) std::cin >> arr[i];
+	std::cout << "Clear!";
+	QSort(arr, 0, n - 1);
+	for (int i = 0; i < n; i++) std::cout << arr[i] << " ";
+	system("pause");
+	return 0;
 }
